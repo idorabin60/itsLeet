8d2a62ddaa5897e3f685f87ad693138031d598b9
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-03-31 01:39:49 +0300

    second leetcode in backtracking ido rabin

---
+++ backtracking/combination_sum.py
@@ -0,0 +1,22 @@
+def solve(numbers: list, target: int):
+    result = []
+
+    def backtracking(index: int, current_array: list, desire_target: int):
+        if desire_target == 0:
+            result.append(current_array.copy())
+            return
+        if index >= len(numbers) or desire_target < 0:
+            return
+
+        current_array.append(numbers[index])
+        backtracking(index, current_array, desire_target - numbers[index])
+        current_array.pop()
+        backtracking(index + 1, current_array, desire_target)
+
+    backtracking(0, [], target)
+    return result
+
+
+print(solve([2, 5, 6, 9], 9))
+
+# by ido rabin
