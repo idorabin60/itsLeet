6d0c7121376ec7781672b8c8a39ec345d7eee88e
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-04-04 20:36:17 +0300

    sliding win

---
+++ sliding_windows/longest_reapting_char_replace.py
@@ -0,0 +1,11 @@
+def char_replace(s: str, k: int) -> int:
+    res = 0
+    l = 0
+    char = {}
+    for right_pointer in range(len(s)):
+        char[s[right_pointer]] = 1 + char.get(s[right_pointer], 1)
+        if (right_pointer-l+1 - max(char.values) > k):
+            char[s[l]] -= 1
+            l += 1
+        res = max(res, right_pointer-l+1)
+    return res
