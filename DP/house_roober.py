ef2baa6d8ef136337d789129e5120fa9b0bfaf44
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-04-29 00:30:45 +0300

    bla

---
+++ DP/house_roober.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+        return self.solve(0, nums, memo)  # removed self from inside
+
+    def solve(self, index, nums, memo):
+        if index in memo:
+            return memo[index]
+        if index >= len(nums):
+            return 0
+        memo[index] = max(self.solve(index + 2, nums, memo) + nums[index],
+                          self.solve(index + 1, nums, memo))
+        return memo[index]
